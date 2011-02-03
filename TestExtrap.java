49fda3cec8cabca199f298e7ab641ddf05a3c9bf
Rami Shomali <rshomali@andrew.cmu.edu>
2011-02-03 16:28:48 -0500

    modified extrapolation to use StandardFilter

---
+++ TestExtrap.java
@@ -0,0 +1,66 @@
+/******************************************************************************************************************
+* File:Plumber.java
+* Course: 17655
+* Project: Assignment 1
+* Copyright: Copyright (c) 2003 Carnegie Mellon University
+* Versions:
+*	1.0 November 2008 - Sample Pipe and Filter code (ajl).
+*
+* Description:
+*
+* This class serves as an example to illstrate how to use the PlumberTemplate to create a main thread that
+* instantiates and connects a set of filters. This example consists of three filters: a source, a middle filter
+* that acts as a pass-through filter (it does nothing to the data), and a sink filter which illustrates all kinds
+* of useful things that you can do with the input stream of data.
+*
+* Parameters: 		None
+*
+* Internal Methods:	None
+*
+******************************************************************************************************************/
+
+import java.util.HashMap;
+import java.util.Vector;
+
+public class TestExtrap
+{
+	   public static void main( String argv[])
+	   {
+			/****************************************************************************
+			* Here we instantiate three filters.
+			****************************************************************************/
+
+			SourceFilter source = new SourceFilter("FlightData.dat");
+			
+			HashMap<Integer, WildPointTest> IDsAndWildPointTests = new HashMap<Integer, WildPointTest>();
+			WildPointTest pressureTest = new PressureWildPointTest();
+			IDsAndWildPointTests.put(ID.PRES, pressureTest);
+			ExtrapolatorFilter extrapolator = new ExtrapolatorFilter(IDsAndWildPointTests);
+			
+			
+			SinkFilter sink = new SinkFilter(null);
+			
+			
+
+			/****************************************************************************
+			* Here we connect the filters starting with the sink filter (Filter 1) which
+			* we connect to Filter2 the middle filter. Then we connect Filter2 to the
+			* source filter (Filter3).
+			****************************************************************************/
+
+			sink.Connect(source); // This esstially says, "connect sink input port to converter output port
+			extrapolator.Connect(source); // This esstially says, "connect converter intput port to source output port
+
+			/****************************************************************************
+			* Here we start the filters up. All-in-all,... its really kind of boring.
+			****************************************************************************/
+
+			
+			source.start();
+			extrapolator.start();
+			sink.start();
+			
+	   } // main
+
+
+} // Plumber
