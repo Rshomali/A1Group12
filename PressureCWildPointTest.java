699fe8080b70a3b4ebda8f9fceb7eb974b9e7d1a
Nan Chen <nanchen@cmu-433356.wv.cc.cmu.edu>
2011-02-05 19:48:09 -0500

    c

---
+++ PressureCWildPointTest.java
@@ -0,0 +1,14 @@
+package hw1;
+
+
+public class PressureCWildPointTest implements WildPointTest {
+
+	@Override
+	public boolean execute(double value) {
+		if( (value > 90) || (value < 45) )
+			return true;
+		else
+			return false;
+	}
+
+}
